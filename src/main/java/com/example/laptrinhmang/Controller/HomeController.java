e5959eac0e6ae6708f9e5ae678722cb082d21504
lamlengend98 <lamnguyen98tb@gmail.com>
2019-03-14 09:31:00 +0700

    CRUD hang hoa

--- src/main/java/com/example/laptrinhmang/Controller/HomeController.java
+++ src/main/java/com/example/laptrinhmang/Controller/HomeController.java
@@ -12,6 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -56,5 +65,56 @@ public class HomeController {
         model.addAttribute("listProduct",productData.getAllProducts());
 
         return "Menu.html";
+    }
+
+    private final Path rootLocation = Paths.get("filestorage");
+    @GetMapping("/admin/add")
+    public String addPro(Model model){
+        model.addAttribute("product", new Product());
+        return "addPro";
+    }
+
+    @GetMapping("/admin/save")
+    public String savePro(Model model, HttpServletRequest request, Product product){
+        System.out.println(request.getParameter("img"));
+        model.addAttribute("product", new Product());
+        product.setName(request.getParameter("name"));
+        product.setPrice(Float.parseFloat(request.getParameter("price")));
+        product.setNumbers(Integer.parseInt(request.getParameter("numbers")));
+        product.setImg(request.getParameter("img"));
+        product.setProperties(request.getParameter("properties"));
+        product.setUsed(request.getParameter("used"));
+        productData.addProduct(product);
+        return "redirect:/admin/home";
+    }
+
+    @GetMapping("/admin/delete/{id}")
+    public String deletePro(@PathVariable int id){
+        System.out.println(id);
+        productData.deleteProduct(id);
+        return "redirect:/admin/home";
+    }
+
+    @GetMapping("/admin/edit/{id}")
+    public String editPro(@PathVariable int id, Product product, HttpServletRequest request, Model model){
+        System.out.println(id);
+        List<Product> list = productData.getAllProducts();
+        model.addAttribute("product", list.get(id));
+
+        return "editPro";
+    }
+
+    String urlfile;
+    @GetMapping("/admin/edit/save")
+    public String saveEditPro( Model model, HttpServletRequest request, Product product){
+
+        product.setName(request.getParameter("name"));
+        product.setPrice(Float.parseFloat(request.getParameter("price")));
+        product.setNumbers(Integer.parseInt(request.getParameter("numbers")));
+        product.setImg(request.getParameter("img"));
+        product.setProperties(request.getParameter("properties"));
+        product.setUsed(request.getParameter("used"));
+        productData.editProduct(Integer.parseInt(request.getParameter("id")), product);
+        return "redirect:/admin/home";
     }
 }
